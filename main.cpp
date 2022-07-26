d538b4274d050921c03468884a0f1226d655bbce
Boris Skurikhin <boriskurikhin@gmail.com>
2022-07-25 23:26:52 -0400

    update
    the old one had a bug

--- main.cpp
+++ main.cpp
@@ -1,138 +1,111 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <algorithm>
 
 using namespace std;
 
-/*
-    X < Y = TRUE;
-    X >= Y = FALSE
-
-    This function compares two strings, and ultimately decides where (unsigned) str1 < str2
-    returns true or false given the above constraint.
-*/
-
-bool smaller(string str1, string str2) {
-    //grab lengths
-    int l1 = str1.size(), l2 = str2.size(); 
-    //check if it's smaller just using the lengths
-    if (l1 < l2) return true;
-    //check if it's bigger just using the lengths
-    if (l1 > l2) return false;
-    //if both strings are the same length, we should iterate through and check digit by digit
-    //starting with the most significant bit!
-    for (int i = 0; i < l1; i++) {
-        if (str1[i] < str2[i]) return true;
-        if (str1[i] > str2[i]) return false;
-    }
-    //they're identical
-    return false;
-}
-
-/*
-    This is the main adder function, takes two strings and adds them into
-    a new answer. Only works with positive numbers
-*/
-string add(string str1, string str2) {
-    //Perfers having the smaller number as the denominator
-    if (str1.size() > str2.size()) swap(str1, str2);
-    int l1 = str1.size(), l2 = str2.size(), diff = l2 - l1, carry = 0;
-    string result = "";
-    //Starts with the least significant bit
-    for (int i = l1 - 1; i >= 0; i--) {
-        //Performs table addition and stores the carry
-        int col = ((str1[i] - '0') + (str2[i + diff] - '0') + carry);
-        result.push_back(col % 10 + '0');
-        carry = col / 10;
-    }
-    //Completes the larger number
-    for (int i = diff - 1; i >= 0; i--) {
-        int col = (str2[i] - '0' + carry);
-        result.push_back(col % 10 + '0');
-        carry = col / 10;
-    }
-    //Checks if there's a carry
-    if (carry) result.push_back(carry + '0');
-    //reverses
-    reverse(result.begin(), result.end());
-    //returns
-    return result;
-}
-
 int N;
-string first, second, secondComp, result;
+pair<string, bool> a, b;
+
+bool bigger(string A, string B) {
+    return A.size() == B.size() ? A.compare(B) > 0 : A.size() > B.size();
+}
+
+string add(string A, string B) {
+    int carry = 0;
+    int i = A.size() - 1;
+    int j = B.size() - 1;
+    string ans = "";
+    
+    while (j >= 0 && i >= 0) {
+        int temp = carry + (A[i--] - '0') + (B[j--] - '0');
+        int add = temp % 10;
+        carry = temp / 10;
+        ans += (char) ('0' + add);
+    }
+
+    while (i >= 0) {
+        int temp = carry + (A[i--] - '0');
+        int add = temp % 10;
+        carry = temp / 10;
+        ans += (char) ('0' + add);
+    }
+    
+    while (j >= 0) {
+        int temp = carry + (B[j--] - '0');
+        int add = temp % 10;
+        carry = temp / 10;
+        ans += (char) ('0' + add);
+    }
+
+    if (carry > 0)
+        ans += (char) ('0' + carry);
+
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+string comp(string x) {
+    string c = "";
+    for (int i = 0; i < x.size(); i++)
+        c += (char) ('0' + (9 - (x[i] - '0')));
+    return c;
+}
+
+string t(string s) {
+    while (s.size() > 1 && s[0] == '0')
+        s.erase(0, 1);
+    return s;
+}
+
+string sub() {
+    //-a b
+    if (a.second) {
+        if (bigger(a.first, b.first)) {
+            cout << "-";
+            return t(comp(add(b.first, comp(a.first))));
+        } else {
+            return t(comp(add(a.first, comp(b.first))));
+        }
+    } else {
+        if (bigger(b.first, a.first)) {
+            cout << "-";
+            return t(comp(add(a.first, comp(b.first))));
+        } else {
+            return t(comp(add(b.first, comp(a.first))));
+        }
+    }
+}
+
+void testcase() {
+    if (a.first[0] == '-') {
+        a.second = true;
+        a.first.erase(0, 1);
+    }
+    
+    if (b.first[0] == '-') {
+        b.second = true;
+        b.first.erase(0, 1);
+    }
+    
+    a.first = t(a.first);
+    b.first = t(b.first);
+
+    if (a.second == b.second) {
+        if (a.second) cout << "-";
+        cout << add(a.first, b.first) << endl;
+    } else {
+        cout << sub() << endl;
+    }
+}
 
 int main() {
-    cin.tie(0); cin.sync_with_stdio(0);
-        cin >> first >> second;
-        secondComp = "";
+    cin.tie(0);
+    cin.sync_with_stdio(false);
 
-        //If both numbers are negative
-        if (first[0] == '-' && second[0] == '-') {
-            result = add(first.substr(1), second.substr(1));
-            //remove leading 0's
-            result.erase(0, result.find_first_not_of('0'));
-            if (result == "") result = "0";
-            //Output the sum, with negative sign
-            cout << (result == "0" ? "" : "-") << result << endl;
-        } else if (first[0] != '-' && second[0] != '-') {
-            //If both numbers are positive
-            result = add(first, second);
-            result.erase(0, result.find_first_not_of('0'));
-            //Remove trailing zeros
-            if (result == "") result = "0";
-            cout << result << endl;
-        } else {
-            int which = 1;
-            //If the first number is negative
-            if (first[0] == '-') {
-                //take out negative sign
-                first = first.substr(1);
-            } else {
-                second = second.substr(1);
-                which = 2;
-            }
-
-            //If youre subtracting two equal numbers...
-            if (first == second) {
-                cout << "0" << endl; 
-                return 0;
-            }
-
-            bool neg = false;
-            //Perfers bigger number in numerator
-            if (smaller(first, second)) {
-                neg = true;
-                swap(first, second);
-            }
-
-            //Pad the smaller number with 9's
-            for (int i = 0; i < (first.size() - secondComp.size()); i++) secondComp= "9" + secondComp;
-            //9's complement
-            for (int i = 0; i < second.size(); i++) secondComp.push_back(9 - (second[i] - '0') + '0');
-            //Add complement to the first number
-            result = add(first, secondComp);
-            //If we have a carry
-            if (result.size() > first.size()) {
-                string carry = to_string(result[0] - '0');
-                result = result.substr(1);
-                //Remove it and add it back into the result
-                result = add(carry, result);
-            } else {
-                //If there's no carry just do 9's complement again
-                for (int i = 0; i < result.size(); i++) result[i] = (9 - (result[i] - '0') + '0');
-            }
-
-            //Remove leading 0's
-            result.erase(0, result.find_first_not_of('0'));
-
-            //0 check
-            if (result == "") {
-                cout << "0" << endl;
-                return 0;
-            }
-
-            cout << (neg ? (which == 2 ? "-" : "") : (which == 1 ? "-" : "")) << result << endl;
-        }
-    return 0;
+    cin >> a.first >> b.first;
+    a.second = false;
+    b.second = false;
+    testcase();
 }
